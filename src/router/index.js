5d5ec87d71b437ea3b3b2eab014ccf16d660f297
Graspme <lsfsfsf@126.com>
2023-12-12 19:32:37 +0800

    增加项目分解路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Quality_CreateProject from '@/views/Quality/Quality_CreateProject'
 import Quality_Project from '@/views/Quality/Quality_Project'
+import Experiment_director1 from '@/views/Experiment/Experiment_director1'
+import Experiment_director2 from '@/views/Experiment/Experiment_director2'
+
 
 const routes = [
   {
@@ -20,7 +23,14 @@ const routes = [
     component: Quality_Project,
   },
   {
-
+    path: '/experimentdepartment/director1/',
+    name: 'director1',
+    component: Experiment_director1,
+  },
+  {
+    path: '/experimentdepartment/director2/',
+    name: 'director2',
+    component: Experiment_director2,
   }
 ]
 
