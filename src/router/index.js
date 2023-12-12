db5cb9ec497e71fb50a4cc52dd9481ba4defb24e
Lewiz <260743926@qq.com>
2023-12-12 22:17:15 +0800

    合并

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Quality_CreateProject from '@/views/Quality/Quality_CreateProject'
 import Quality_Project from '@/views/Quality/Quality_Project'
-<<<<<<< HEAD
 import Quality_StandardList from '@/views/Quality/Quality_StandardList'
-=======
 import Experiment_director1 from '@/views/Experiment/Experiment_director1'
 import Experiment_director2 from '@/views/Experiment/Experiment_director2'
 
->>>>>>> 8b4fe69dde631d554ab5ca4df5109bf43cc7aa92
 
 const routes = [
   {
@@ -27,11 +24,11 @@ const routes = [
     component: Quality_Project,
   },
   {
-<<<<<<< HEAD
     path: '/qualitydepartment/standardlist',
     name: 'standardlist',
     component: Quality_StandardList,
-=======
+  },
+  {
     path: '/experimentdepartment/director1/',
     name: 'director1',
     component: Experiment_director1,
@@ -40,7 +37,6 @@ const routes = [
     path: '/experimentdepartment/director2/',
     name: 'director2',
     component: Experiment_director2,
->>>>>>> 8b4fe69dde631d554ab5ca4df5109bf43cc7aa92
   }
 ]
 
