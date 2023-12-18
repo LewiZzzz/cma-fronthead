b587efeed5facd1c571752c52b0c92469a156098
wb <492686517@qq.com>
2023-12-18 22:36:46 +0800

    加载设备科页面；新增设备部分上传pdf还有点没整好

--- src/views/Device/Device_Management.vue
+++ src/views/Device/Device_Management.vue
@@ -1,14 +1,52 @@
 <template>
   <ContentBase>
-    <el-table :data='devices' stripe style="width: 100%" height="250" :border='true' :header-cell-style="{'text-align':'center'}" :cell-style="{'text-align':'center'}" >
-      <el-table-column fixed:true prop="id" label="仪器设备编号" width="180" />
-      <el-table-column prop="name" label="名称" width="180" />
-      <el-table-column prop="parameter" label="型号/规格/等级" width="180"/>
-      <el-table-column prop="range" label="测量范围" width="180"/>
-      <el-table-column prop="year" label="购买年份" width="180" />
-      <el-table-column prop="trace" label="溯源方式" width="180" />
-      <el-table-column prop="expirationdate" label="有效截止日期至" width="180" />
-      <el-table-column prop="source" label="设备来源" width="180" />
+    <el-table :data='equipList' stripe style="width: 100%" height="250" :border='true' :header-cell-style="{'text-align':'center'}" :cell-style="{'text-align':'center'}" >
+      <el-table-column fixed:true prop="projectId" label="项目编号" width="180" />
+      <el-table-column prop="equipId" label="仪器设备编号" width="180" />
+      <el-table-column prop="equipName" label="名称" width="180" />
+      <el-table-column prop="specification" label="型号/规格/等级" width="180"/>
+      <el-table-column prop="measureRange" label="测量范围" width="180"/>
+      <el-table-column prop="purchaseYear" label="购买年份" width="180" />
+      <el-table-column  prop="traceWay" label="溯源方式" width="180" >
+        <template #default="scope">
+            <span>{{ scope.row.state === 1 ?'检定':(scope.row.state === 2 ? '校准' : (scope.row.state === 3 ? '内部校准' : '其他方式'))}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="validDate" label="有效截止日期至" width="180" />
+      <el-table-column prop="equipmentSource" label="设备来源" width="180" />
+      <el-table-column  prop="qualityCertificate" label="设备计量证书" width="180" >
+        <template #default="scope">
+          <el-link type="danger" :href="scope.row.qualityCertificate">
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+               class="bi bi-filetype-pdf" viewBox="0 0 16 16">
+               <path fill-rule="evenodd"
+                    d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z" />
+             </svg>
+          </el-link>
+        </template>
+      </el-table-column>
+      <el-table-column  prop="operationSpecification" label="仪器操作规范" width="180" >
+        <template #default="scope">
+          <el-link type="danger" :href="scope.row.operationSpecification">
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+               class="bi bi-filetype-pdf" viewBox="0 0 16 16">
+               <path fill-rule="evenodd"
+                    d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z" />
+             </svg>
+          </el-link>
+        </template>
+      </el-table-column>
+      <el-table-column  prop="userTable" label="仪器授权使用人员一览表" width="180" >
+        <template #default="scope">
+          <el-link type="danger" :href="scope.row.userTable">
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+               class="bi bi-filetype-pdf" viewBox="0 0 16 16">
+               <path fill-rule="evenodd"
+                    d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z" />
+             </svg>
+          </el-link>
+        </template>
+      </el-table-column>
       <el-table-column fixed="right" label="操作" width="120">
       <template #default>
         <el-button link type="primary" size="small">修改</el-button>
@@ -61,13 +99,31 @@
                            <el-input v-model="equip.equipmentSource" placeholder="" style="width: 235px"></el-input>
                       </el-form-item>
                       <el-form-item label="设备计量证书">
-                           
+                        <el-upload v-model="equip.validDate" ref="upload" class="upload-demo"
+                                    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" :limit="1"
+                                    :on-exceed="handleExceed" :auto-upload="false">
+                                    <template #trigger>
+                                        <el-button type="primary">选择pdf文件</el-button>
+                                    </template>
+                        </el-upload> 
                       </el-form-item>
                       <el-form-item label="仪器操作规范">
-                           
+                        <el-upload ref="upload" class="upload-demo"
+                                    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" :limit="1"
+                                    :on-exceed="handleExceed" :auto-upload="false">
+                                    <template #trigger>
+                                        <el-button type="primary">选择pdf文件</el-button>
+                                    </template>
+                        </el-upload> 
                       </el-form-item>
                       <el-form-item label="仪器授权使用人员一览表"  >
-                           
+                        <el-upload ref="upload" class="upload-demo"
+                                    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" :limit="1"
+                                    :on-exceed="handleExceed" :auto-upload="false">
+                                    <template #trigger>
+                                        <el-button type="primary">选择pdf文件</el-button>
+                                    </template>
+                        </el-upload> 
                       </el-form-item>
                       <el-form-item>
                         <el-button type="primary" @click="addEquip">提交</el-button>
@@ -98,7 +154,7 @@
  <script>
  import ContentBase from '@/components/ContentBase'
 import {
-// ref,
+ref,
 reactive,
 } from 'vue'
  import $ from 'jquery'
@@ -108,6 +164,26 @@ reactive,
          ContentBase: ContentBase,
      },
      setup() {
+
+      const equipList=ref([]);
+      const loadEquipList=()=>{
+        $.ajax({
+          url:'',
+          type:'GET',
+          dataType:'json',
+          success:(resp)=>{
+            if(resp.code===1){
+              equipList.value=resp.data;
+              console.log('获取数据成功')
+            }else{
+              console.log('获取数据失败')
+            }
+          },
+          error:(error)=>{
+            console.log(error);
+          },
+        });
+      };
       const equip =reactive({
           projectId:0,
           equipId: '', // 设备唯一标识
@@ -125,23 +201,24 @@ reactive,
 
       return {
           equip,
+          equipList,
+          loadEquipList,
+
       };
      },
      data() {
      return {
-      //设备详细信息
-      deviceInfor: {
-              id: '',
-              name: '',
-              parameter: '',
-              range: '',
-              year: 0,
-              trace: '',
-              expirationdate: '',
-              source: '',
-          }, 
-    
-       
+      // 设备详细信息
+      // deviceInfor: {
+      //         id: '',
+      //         name: '',
+      //         parameter: '',
+      //         range: '',
+      //         year: 0,
+      //         trace: '',
+      //         expirationdate: '',
+      //         source: '',
+      //     },     
      };
    },
    methods: {
